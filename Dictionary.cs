f35ffe171e33831897c0d7d0490d40547f0ddca7
Ömer Faruk YILMAZ <109068669+ArPharafin@users.noreply.github.com>
2023-02-25 02:48:44 +0300

    Add project files.

---
+++ Dictionary.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace MyDictionary
+{
+    internal class Dictionary
+    {
+        Dictionary<int,string>myDictionary = new Dictionary<int,string>();
+        public void Add()
+        {
+            myDictionary.Add(1,"Omer" );
+            myDictionary.Add(2,"Seyma");
+            myDictionary.Add(3,"Kerem");
+            myDictionary.Add(4,"Rumeysa");
+            foreach(KeyValuePair<int,string>kvp in myDictionary)
+            {
+                Console.WriteLine(kvp);
+            }
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary  dictionary = new Dictionary();
+        }
+    }
+}
